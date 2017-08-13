ff94f6da51b4b8dbf69d51ca77ece417a62154a2
Allan Passos <allansuru@hotmail.com>
2017-08-13 16:47:08 -0300

    Adicionando boas praticas!

--- src/EP.IdentityIsolation.MVC/Utilidades/SQL_Uteis_Iniciais/Starts.sql
+++ src/EP.IdentityIsolation.MVC/Utilidades/SQL_Uteis_Iniciais/Starts.sql
@@ -1,24 +1,24 @@
 ﻿insert into AspNetRoles values(1,'Admin')
-
+GO
 
 select * from AspNetRoles
-
+GO
 select * from AspNetUserRoles
-
+GO
 insert into AspNetUserRoles values('42fcc459-1d94-4436-97e4-1d25146c7bab',1)
-
+GO
 
 
 select * from AspNetUsers where  AspNetUsers.Id = '42fcc459-1d94-4436-97e4-1d25146c7bab'
-
+GO
 
 insert into AspNetClaims  values(NEWID(),'AdmClaims')
-
+GO
 select * from AspNetClaims
-
+GO
 
 select * from AspNetUserClaims
-
+GO
 
 CREATE TABLE AspNetRoles
 (
@@ -26,3 +26,4 @@ Id int,
 Name varchar(255)
 
 );
+GO
